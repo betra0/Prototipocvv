a69cebb40afa64297b06e77e3697192406fdf1cb
Danielhp <tolozadaniel5@gmail.com>
2024-02-08 20:35:14 -0300

    agrege un boton para mobile

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -59,3 +59,28 @@ function ir_comentarioleft() {
 }
 document.getElementById('btn-c-left').addEventListener('click', ir_comentarioleft);
 document.getElementById('btn-c-rigth').addEventListener('click', ir_comentarioright);
+
+let btn = document.getElementById('btn-menu');
+let header = document.getElementById('header');
+btn.addEventListener('click', function() {
+    header.classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Cerrar el menú si está abierto
+        if (header.classList.contains('active')) {
+            header.classList.remove('active');
+        }
+
+        const href = this.getAttribute('href');
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        window.scrollTo({
+            top: offsetTop - 50, // 50px por encima del elemento destino
+            behavior: 'smooth'
+        });
+    });
+});
